c4fed7299c55e5f6be188e2f92a89ccc88e16f17
DanielCripps94 <dan-cripps@hotmail.co.uk>
2020-03-16 12:52:41 +0000

    test back-end finished

--- mongo-backend/routes/projects.js
+++ mongo-backend/routes/projects.js
@@ -23,6 +23,33 @@ router.route('/add').post((req, res) => {
     newProject.save()
     .then((res.json)('Project added!'))
     .catch(err => res.status(400).json("Error " + err))
+});
+
+router.route('/:id').get((req, res) => {
+    Project.findById(req.params.id)
+    .then(project => res.json(project))
+    .catch(err => res.status(400).json("Error " + err))
+});
+
+router.route('/:id').delete((req, res) => {
+    Project.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Project Deleted!"))
+    .catch(err => res.status(400).json("Error " + err))
 })
+
+router.route('/update/:id').post((req, res) => {
+    Project.findById(req.params.id)
+        .then(project => {
+            project.username = req.body.username;
+            project.title = req.body.title;
+            project.length = Number(req.body.length);
+            project.date = Date.parse(req.body.date);
+
+            project.save()
+                .then(() => res.json("Project Updated!"))
+            .   then(err => res.status(400).json("Error " + err));
+        })
+        .catch(err => res.status(400).json("Error " + err))
+});
 
 module.exports = router;
